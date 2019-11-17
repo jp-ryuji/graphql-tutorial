f0b637481ba1b9ebc5e52bcdd474ccaf503f11a1
Ryuji Eguchi <eguchi.ryuji@gmail.com>
2019-11-17 20:33:33 +0900

    Add field :all_links to QueryType

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,11 +2,12 @@ module Types
   class QueryType < Types::BaseObject
     # queries are just represented as fields
     # `all_links` is automatically camelcased to `allLinks`
-    field :all_links, [LinkType], null: false
+    # field :all_links, [LinkType], null: false
+    field :all_links, function: Resolvers::LinksSearch
 
     # this method is invoked, when `all_link` fields is being resolved
-    def all_links
-      Link.all
-    end
+    # def all_links
+    #   Link.all
+    # end
   end
 end
